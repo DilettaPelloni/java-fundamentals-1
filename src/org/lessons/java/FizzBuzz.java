483e9c34f061057b427408179402a749fe128403
Diletta Pelloni <dilpel@live.it>
2023-05-31 15:10:17 +0200

    Implementato fizzbuzz

---
+++ src/org/lessons/java/FizzBuzz.java
@@ -0,0 +1,16 @@
+package org.lessons.java;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        //stampo i numeri da 1 a 100
+        for (int i = 1; i <= 100; i++) {
+            if(i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
