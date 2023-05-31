e135719e4cba40fcca928aa281b9a6de1f0184ee
Diletta Pelloni <dilpel@live.it>
2023-05-31 15:14:43 +0200

    Esercizio completato

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -2,11 +2,17 @@ package org.lessons.java;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        //stampo i numeri da 1 a 100
-        for (int i = 1; i <= 100; i++) {
-            if(i % 3 == 0) {
+
+        //Stabilisco il totale dei numeri da mostrare
+        int totalNumbers = 100;
+
+        //stampo i numeri da 1 a totalNumbers
+        for (int i = 1; i <= totalNumbers; i++) {
+            if((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
-            } else if (i % 5 == 0) {
+            }  else if (i % 5 == 0) {
                 System.out.println("Buzz");
             } else {
                 System.out.println(i);
