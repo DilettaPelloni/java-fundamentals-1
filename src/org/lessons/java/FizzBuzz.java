b42def714ff3b92194905375396f23dd9d17708a
Diletta Pelloni <dilpel@live.it>
2023-05-31 15:29:02 +0200

    Esercizio completato con bonus

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -1,10 +1,37 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main(String[] args) {
 
-        //Stabilisco il totale dei numeri da mostrare
-        int totalNumbers = 100;
+        //creo un'istanza di scanner
+        Scanner getInput = new Scanner(System.in);
+
+        //stabilisco il numero minimo e massimo
+        int minNum = 0;
+        int maxNum = 1000;
+
+        //chiedo all'utente quanti numeri vuole stampare
+        System.out.println("Quanti numeri vuoi stampare?");
+        System.out.println("Mi raccomando, devono essere più di " + minNum + " e meno di " + maxNum);
+
+        //inizializzo una variabile in cui inserire i numeri da mostrare
+        int totalNumbers;
+
+        do {
+            //recupero il dato dell'utente
+            totalNumbers = getInput.nextInt();
+            //messaggio di errore se il numero è troppo piccolo
+            if(totalNumbers < minNum) {
+                System.out.println("Il numero non può essere minore di " + minNum + "!");
+            }
+            //messaggio di errore se il numero è troppo grande
+            if(totalNumbers > maxNum) {
+                System.out.println("Il numero non può essere maggiore di " + (maxNum - 1) + "!");
+            }
+        } while (totalNumbers < minNum || totalNumbers > maxNum );
+
 
         //stampo i numeri da 1 a totalNumbers
         for (int i = 1; i <= totalNumbers; i++) {
